123da2342296c48f8ceaaa97035f603a58dae89a
desolt <desolt@protonmail.com>
2017-07-16 00:41:00 -0400

    toml: added more table functionality

--- include/toml.h
+++ include/toml.h
@@ -7,6 +7,8 @@
 
 typedef struct toml_table toml_table;
 typedef struct toml_array toml_array;
+typedef struct toml_pair toml_pair;
+typedef struct toml_node toml_node;
 
 typedef enum toml_err
 {
@@ -23,6 +25,19 @@ typedef enum toml_type
 	TOML_FLOAT,
 	TOML_BOOL
 } toml_type;
+
+struct toml_table
+{
+	char * name; // NULL if root.
+	toml_pair * pairs;
+};
+
+struct toml_array
+{
+	toml_type type; // Arrays in toml only support one type, though it can be arrays.
+	toml_node * values;
+};
+
 
 // TODO: UTF-8
 typedef struct toml_value
@@ -47,7 +62,16 @@ typedef struct toml_value
 	};
 } toml_value;
 
+toml_table * toml_init();
+/// Every table requires a parent & name, except for the root table.
+toml_table * toml_create_table(const char * name, toml_table * parent);
+bool toml_table_has_child(const toml_table * table, const char * name);
+
+/// @return the value associated with the key. NULL if does not exist.
+toml_value * toml_table_get(const toml_table * table, const char * key);
+toml_value * toml_array_at(const toml_array * array, size_t index);
+
 /// @return The root table. name will be a null pointer.
-toml_table * toml_parse(const char * src);
+toml_err toml_parse(const char * src, toml_table ** root);
 
 #endif // _TOML_H
--- src/toml.c
+++ src/toml.c
@@ -1,19 +1,6 @@
 #include "toml.h"
-
-typedef struct toml_pair toml_pair;
-typedef struct toml_node toml_node;
-
-struct toml_table
-{
-	char * name; // NULL if root.
-	toml_pair * pairs;
-};
-
-struct toml_array
-{
-	toml_type type; // Arrays in toml only support one type, though it can be arrays.
-	toml_node * values;
-};
+#include <stdlib.h>
+#include <string.h>
 
 struct toml_node
 {
@@ -28,7 +15,88 @@ struct toml_pair
 	toml_pair * next, *prev;
 };
 
-toml_table * toml_parse(const char * src)
+toml_err toml_table_emplace(toml_table * table, const char * key, toml_value *val, toml_pair ** out);
+
+toml_table * toml_init()
 {
-	return NULL;
+	toml_table *root = calloc(1, sizeof(toml_table));
+	if (root == NULL) return NULL;
+
+	root->name = NULL;
+	root->pairs = NULL;
+
+	return root;
+}
+
+toml_table * toml_create_table(const char *name, toml_table * parent)
+{
+	// these are required, don't hackishly create a root table.
+	if (parent == NULL || name == NULL) return NULL;
+
+	toml_table * table = calloc(1, sizeof(toml_table));
+	if (table == NULL) return NULL;
+
+	table->name = strdup(name);
+	table->pairs = NULL;
+
+	// insert the new table into the parent
+	toml_value *table_val = calloc(1, sizeof(toml_value));
+	table_val->type = TOML_TABLE;
+	table_val->table_val = table;
+	toml_table_emplace(parent, name, table_val, NULL);
+
+	return table;
+}
+
+bool toml_table_has_child(const toml_table * table, const char * name)
+{
+	toml_value * val = toml_table_get(table, name);
+	if (val == NULL) return false;
+
+	return val->type == TOML_TABLE; // not the same as has_key
+}
+
+toml_value * toml_table_get(const toml_table * table, const char * key)
+{
+	toml_pair *pair = table->pairs;
+	if (pair == NULL) return NULL;
+
+	while (pair != NULL) 
+	{
+		if (!strcmp(key, pair->key)) 
+		{
+			return pair->val;
+		}
+
+		pair = pair->next;
+	}
+
+	return false;
+}
+
+/// doesn't check if the key exists already
+toml_err toml_table_emplace(toml_table * table, const char * key, toml_value *val, toml_pair ** out)
+{
+	toml_pair *pair = calloc(1, sizeof(toml_pair));
+	pair->key = strdup(key);
+	pair->val = val;
+
+	toml_pair *node = table->pairs;
+	if (node != NULL)
+	{
+		while (node->next != NULL) node = node->next; // find end of linked list
+		node->next = pair;
+	} else
+	{
+		table->pairs = pair;
+	}
+
+	if (out != NULL) *out = pair;
+
+	return toml_success;
+}
+
+toml_err toml_parse(const char * src, toml_table ** out)
+{
+	return toml_success;
 }
